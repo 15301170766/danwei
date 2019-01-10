4706b20f42a6b2f2ce8599d04a6192c286372dba
15301170766 <15301170766@163.com>
2019-01-10 15:45:18 +0800

    加入vue-class，Login修改为ts模式

--- danwei/src/components/HelloWorld.vue
+++ danwei/src/components/HelloWorld.vue
@@ -7,114 +7,123 @@
       <div class="top">
         <h3>登录</h3>
       </div>
-    <el-form :model="rulesForm" status-icon :rules="rules" ref="rulesForm" label-width="60px" class="demo-ruleForm">
-       <el-form-item label="账号" prop="count">
-      <el-input v-model.number="rulesForm.count"></el-input>
-    </el-form-item>
-    <el-form-item label="密码" prop="password">
-      <el-input type="password" autocomplete="off" v-model.number="rulesForm.password"></el-input>
-    </el-form-item>
-   
-    <el-form-item>
-      <el-button type="primary" @click="submitForm('rulesForm')">提交</el-button>
-      <el-button @click="resetForm('rulesForm')">重置</el-button>
-    </el-form-item>
-  </el-form>
+      <el-form
+        :model="rulesForm"
+        status-icon
+        :rules="rules"
+        ref="rulesForm"
+        label-width="60px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="账号" prop="count">
+          <el-input v-model.number="rulesForm.count"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input type="password" autocomplete="off" v-model.number="rulesForm.password"></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('rulesForm')">提交</el-button>
+          <el-button @click="resetForm('rulesForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
 
-<script>
-import { Loading } from 'element-ui';
-export default {
-  name: 'HelloWorld', 
-   data(){
-       var CountValid = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请输入账号'));
-        } else {
-          if (this.rulesForm.count !== '') {
-                if (!Number.isInteger(value)) {
-                callback(new Error('请输入数字值'));
-              } 
-              callback();
+<script lang='ts'>
+import Vue from "vue" ;
+import Component from "vue-class-component";
+import { Loading } from "element-ui";
+
+@Component
+export default class Hello extends Vue {
+    name:string= "HelloWorld";
+     CountValid = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入账号"));
+      } else {
+        if (this.rulesForm.count !== "") {
+          if (!Number(value)) {
+            callback(new Error("请输入数字值"));
           }
           callback();
         }
-      };
-      var PassWordValid = (rule, value, callback) =>{
-        if(value === ""){
-          callback(new Error('请输入密码'));
-        } else {
-             if (!Number.isInteger(value)) {
-                callback(new Error('请输入数字值'));
-              }
-          callback(); 
-        }
+        callback();
       }
-    return{
-            rulesForm:{
-            count:"",
-            password:""
-          },
-     //定义规则
-     rules:{
-       count:[
-         {validator:CountValid,trigger:"blur"},//定义规则
-       ],
-       password:[
-         {validator:PassWordValid,trigger:"blur"},//定义规则
-       ]
-     }
-    }
-   },
-   methods:{
-     submitForm(formName){
-           this.$refs[formName].validate((valid) => {
-          if (valid) {
-            if(this.rulesForm.count === 666666 || this.rulesForm.password === 666666){
-             let LoadingLogin = Loading.service({
-                   lock: true,
-                    text: '登录成功请稍等',
-                    spinner: 'el-icon-loading',
-                    background: 'rgba(0, 0, 0, 0.7)'
-              });
-              setTimeout(() => {
-                this.$router.push({ path: '/Houtai' });//页面跳转
-                LoadingLogin.close();//loading 关闭
-              }, 2000);
-              
-            }
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
-     },
-     resetForm(resetForm){
-        this.$refs[resetForm].resetFields();
-     }
-   }
-}
+    };
+      PassWordValid = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        if (!Number(value)) {
+          callback(new Error("请输入数字值"));
+        }
+        callback();
+      }
+    };
+
+    rulesForm ={
+        count:null,
+        password: null
+      };
+      //定义规则
+      rules= {
+        count: [
+          { validator: this.CountValid, trigger: "blur" } //定义规则
+        ],
+        password: [
+          { validator: this.PassWordValid, trigger: "blur" } //定义规则
+        ]
+      };
+          mounted () {}
+            resetForm(resetForm:string):any {
+                let ss:any= this;
+                ss.$refs[resetForm].resetFields();
+              }
+
+        submitForm(formName) {
+          let ss:any =this;
+                ss.$refs[formName].validate(valid => {
+                  if (valid) {
+                    if (this.rulesForm.count === 666666 ||this.rulesForm.password === 666666) {
+                      let LoadingLogin = Loading.service({
+                        lock: true,
+                        text: "登录成功请稍等",
+                        spinner: "el-icon-loading",
+                        background: "rgba(0, 0, 0, 0.7)"
+                      });
+                      setTimeout(() => {
+                        this.$router.push({ path: "/Houtai" }); //页面跳转
+                        LoadingLogin.close(); //loading 关闭
+                      }, 2000);
+                    }
+                  } else {
+                    console.log("error submit!!");
+                    return false;
+                  }
+                });
+              };
+      };
 </script>
 
 <style scoped lang="less">
-.hello{
+.hello {
   width: 100%;
   height: 100vh;
   box-sizing: border-box;
   background: url(../assets/timg2.jpg) no-repeat;
-  background-size:100%;
+  background-size: 100%;
 }
-.header{
+.header {
   text-align: center;
   padding-top: 16vh;
-  h1{
+  h1 {
     letter-spacing: 20px;
     color: green;
   }
 }
-.Login{
+.Login {
   margin-top: 10vh;
   float: right;
   border: 1px solid pink;
@@ -126,19 +135,19 @@ export default {
   padding: 20px;
   padding-right: 40px;
   box-sizing: border-box;
-  .top{
+  .top {
     text-align: center;
     margin-bottom: 20px;
-    .el-input{
+    .el-input {
       width: 80%;
       margin-top: 20px;
     }
-    
-  .el-button{
-    margin-top: 20px;
+
+    .el-button {
+      margin-top: 20px;
+    }
   }
-  }
-  h5{
+  h5 {
     margin-top: 20px;
     text-align: center;
     color: red;
